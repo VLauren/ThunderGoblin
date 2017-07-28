ccdf6d999a3d60ba4083235dfd13aaa7ef880621
Lauren <lau.16.9@gmail.com>
2017-07-28 21:14:16 +0200

    cosas básicas

---
+++ Assets/Scripts/Goblin.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goblin : MonoBehaviour
+{
+    public enum Jugador
+    {
+        UNO,
+        DOS
+    }
+
+    public Jugador jugador;
+
+    private CharacterController cc;
+
+    private void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        Debug.Log("Horizontal " + Input.GetAxisRaw("Horizontal"));
+        Debug.Log("Vertical " + Input.GetAxisRaw("Vertical"));
+        Debug.Log("Horizontal2 " + Input.GetAxisRaw("Horizontal2"));
+        Debug.Log("Vertical2 " + Input.GetAxisRaw("Vertical2"));
+
+    }
+
+    private void FixedUpdate()
+    {
+        cc.Move(Vector3.right * Time.fixedDeltaTime);
+    }
+}
