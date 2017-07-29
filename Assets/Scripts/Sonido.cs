fda55f02207df4646866f3b2a8c03cbbfa9521b7
Lauren <lau.16.9@gmail.com>
2017-07-29 20:11:21 +0200

    pararrayos y cosas

---
+++ Assets/Scripts/Sonido.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sonido : MonoBehaviour
+{
+    public static Sonido instancia { get; private set; }
+
+    private void Awake()
+    {
+        instancia = this;
+    }
+
+    public void Play(string sonido, float vol = 1)
+    {
+        GetComponent<AudioSource>().PlayOneShot(Resources.Load(sonido) as AudioClip, vol);
+    }
+}
