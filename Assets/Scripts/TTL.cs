bd0efc7d732397524cee0fff681837ab2882466a
Lauren <lau.16.9@gmail.com>
2017-07-30 13:23:43 +0200

    fix ttl

--- Assets/Scripts/TTL.cs
+++ Assets/Scripts/TTL.cs
@@ -4,14 +4,22 @@ using System.Collections;
 public class TTL : MonoBehaviour
 {
     public float tiempo = 1;
+    private float tInicio;
 
 	void Start ()
     {
-	    Invoke("Destruir",tiempo);
+        tInicio = Time.time;
+	    //Invoke("Destruir",tiempo);
 	}
 	
 	void Destruir ()
     {
         Destroy(gameObject);
 	}
+
+    private void Update()
+    {
+        if (Time.time > tInicio + tiempo)
+            Destruir();
+    }
 }
