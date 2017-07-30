61c984b24d49682b5ae7ef62c85e12f9d699332b
Bardolimo <spake_ice@hotmail.com>
2017-07-30 09:57:23 +0200

    sonidos

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour 
+{
+
+    public Vector3 direction;
+    public float angularRotation;
+
+    private void FixedUpdate()
+    {
+        transform.Rotate(direction * angularRotation * Time.fixedDeltaTime);
+    }
+
+}
