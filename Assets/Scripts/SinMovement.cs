8da95e1e9eb190f7e87b6368f63ad83b3334cb5f
Bardolimo <spake_ice@hotmail.com>
2017-07-30 12:40:15 +0200

    escenario giratorio

---
+++ Assets/Scripts/SinMovement.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class SinMovement : MonoBehaviour 
+{
+
+    public float freq = 1;
+    public bool autoOffset = true;
+
+    [Space]
+    public Vector3 positionA;
+    public Vector3 positionB;
+
+    private float offset;
+
+    #region monobehaviour
+
+    void Awake () 
+	{
+        offset = Random.Range(0, Mathf.PI);
+
+        UpdatePosition();
+    }
+	
+	void Update () 
+	{
+        UpdatePosition();
+	}
+
+    #endregion
+
+    [ContextMenu("Set position A")]
+    void SetPositionA()
+    {
+        positionA = transform.localPosition;
+    }
+
+    [ContextMenu("Set position B")]
+    void SetPositionB()
+    {
+        positionB = transform.localPosition;
+    }
+
+    void UpdatePosition()
+    {
+        float sin = (1f + Mathf.Sin(2 * Mathf.PI * freq * Time.time + offset)) * .5f;
+
+        transform.localPosition = Vector3.Lerp(positionA, positionB, sin);
+    }
+	
+}
